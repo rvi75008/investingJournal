c14646634a1a98ea0bc5520e645fe494d14457e7
Rapha <raphael.vignes@laposte.net>
2024-02-04 16:53:06 +0100

    feat: api

--- src/api.ts
+++ src/api.ts
@@ -22,49 +22,46 @@ app.post('/entries', async (req: express.Request, res: express.Response) => {
 });
 
 app.get('/entries', async (req: express.Request, res: express.Response) => {
-  const entries: JournalEntry[] = await journalDataAccess.getEntries();
+  const entries: JournalEntry[] = await journalDataAccess.getEntries({prisma: prisma});
   res.send(entries);
 });
 
-app.get('/entries/:id', async (req, res) => {
+app.get('/entries/:id', async (req: express.Request, res: express.Response) => {
   const entryId = parseInt(req.params.id, 10);
 
   // Check if entry exists
-  const matchingEntry = await journalDataAccess.getEntryById(entryId);
+  const matchingEntry = await journalDataAccess.getEntryById(entryId, {prisma: prisma});
   if (!matchingEntry) {
     res.status(404).send(`Entry with ID ${entryId} not found`);
     return;
   }
-
-  // Send JSON response with the matching entry
   res.send(matchingEntry);
 });
 
-app.delete('/entries/:id', async (req, res) => {
+app.delete('/entries/:id', async (req: express.Request, res: express.Response) => {
   const entryId = parseInt(req.params.id, 10);
 
-  // Check if entry exists
-  const entry = await journalDataAccess.getEntryById(entryId);
+  const entry = await journalDataAccess.getEntryById(entryId, {prisma: prisma});
   if (!entry) {
     res.status(404).send(`Entry with ID ${entryId} not found`);
     return;
   }
-
-  // Remove the entry from the data store
-  await journalDataAccess.deleteEntry(entryId);
-
-  // Send confirmation message
+  await journalDataAccess.deleteEntry(entryId, {prisma: prisma});
   res.send('Entry deleted successfully');
 });
 
-app.put('/entries/:id', async (req, res) => {
-  const entryId = parseInt(req.params.id, 10);
-  const updatedEntry: JournalEntry = req.body;
-
-
-
-  // Send confirmation message
-  res.send('Entry updated successfully');
+app.put('/entries/:id', async (req: express.Request, res: express.Response) => {
+  try {
+    const result = await journalDataAccess.updateEntry(
+      parseInt(req.params.id, 10),
+      { prisma: prisma },
+      req.body,
+    )
+    res.send('Entry updated sccessfully');
+  }
+  catch(error) {
+    res.status(404).send(error.message);
+  }
 });
 
 app.listen(3000, () => {
